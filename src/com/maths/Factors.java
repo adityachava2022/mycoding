85689911e9fe09e2122ae28ba60e812a598d3e24
caditya <caditya@opentext.com>
2023-01-02 17:54:59 +0530

    factors problem

---
+++ src/com/maths/Factors.java
@@ -0,0 +1,33 @@
+package com.maths;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factors {
+  public static void main(String args[]) {
+    int n = 40;
+    List<Integer> factors = factorsOfN(n);
+    System.out.println(factors);
+  }
+
+  public static List<Integer> factorsOfN(int n) {
+    List<Integer> factors = new ArrayList<>();
+    int i; // declaring i in outer scope
+    for (i = 1; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) {
+        factors.add(i);
+      }
+    }
+    // now decrementing i to calculate quotients
+    if (i - (n / i) == 1) {
+      i--;
+    }
+    for (; i >= 1; i--) // quotient of numbers which give remainder as 0
+    {
+      if (n % i == 0) {
+        factors.add(n / i);
+      }
+    }
+    return factors;
+  }
+}
